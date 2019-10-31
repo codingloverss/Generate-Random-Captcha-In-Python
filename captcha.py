f24959f83aef1051ccce9642d759af90b211e331
Hacker's AdDa <dharmndra076@gmail.com>
2019-10-31 15:01:30 +0530

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,18 +1,26 @@
 import random
 from captcha.image import ImageCaptcha
 def main():
-    a=random.choice(alpha)
-    A=random.choice(ALPHA)
-    n1=random.choice(num)
-    n2=random.choice(num)
-    n3=random.choice(num)
-    row_captcha=a+str(n3)+A+str(n2)+A+str(n1)
-    row_captcha=list(row_captcha)
+    alpha = []
+    ALPHA=[]
+    num=[]
+    for i in range(65,91):
+        ALPHA.append(chr(i))
+        alpha.append(chr(i).lower())
+    for i in range(0,10):
+        num.append(i)
+    s_alpha = random.choice(alpha)
+    c_alpha = random.choice(ALPHA)
+    num1 = random.choice(num)
+    num2 = random.choice(num)
+    num3 = random.choice(num)
+    row_captcha = s_alpha+str(num3)+c_alpha+str(num2)+A+str(num1)
+    row_captcha = list(row_captcha)
     random.shuffle(row_captcha)
-    row_captcha=''.join(row_captcha)
+    row_captcha = ''.join(row_captcha)
     img = ImageCaptcha()
     image = img.generate_image(row_captcha)
     image.show()
-    return a,row_captcha
+    return row_captcha
 if __name__=="__main__":
     main()
